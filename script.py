4156ef06052238879d5381365cf98ea075319893
bpai12345 <124864896+bpai12345@users.noreply.github.com>
2023-09-27 16:32:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,71 +1,65 @@
 import streamlit as st
-import fitz
 import os
-from ArabicOcr import arabicocr
-from io import BytesIO
+import fitz  # PyMuPDF
 from PIL import Image
+from ArabicOcr import arabicocr
+from PIL import Image, ImageOps
 
-# Create a Streamlit app title
-st.title("PDF to Image and Arabic Text Extraction")
+#Make streamlit to wide screen
+st.set_page_config(layout="wide")
+
+# Hide made with streamlit and Hamburger menu
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+
+# Uploading file in the form of pdf, doc and docx format 
+image = Image.open(r'BPAI_logo.png')
+st.image(image)
+st.header("ARABIC PARSER")
 
 # Create a file upload widget
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 # Check if a file is uploaded
 if uploaded_file:
-    # Read the uploaded PDF file
-    pdf_content = uploaded_file.read()
-
     # Save the uploaded PDF as a temporary file
-    with open("arabic_image.pdf", "wb") as temp_pdf:
-        temp_pdf.write(pdf_content)
+    with open("arabic_pdf.pdf", "wb") as temp_pdf:
+        temp_pdf.write(uploaded_file.read())
 
-    # Open the uploaded PDF file
-    doc = fitz.open("arabic_image.pdf")
+    # Open the PDF using PyMuPDF (Fitz)
+    pdf_document = fitz.open("arabic_pdf.pdf")
 
-    # Create columns for layout
-    col1, col2 = st.columns(2)
+    # Create a folder to store images
+    os.makedirs("pdf_images", exist_ok=True)
 
-    # Select a page
-    with col1:
-        # Create a dropdown to select a page
-        page_number = st.selectbox("Select a Page", range(len(doc)))
+    # Convert PDF pages to images
+    image_paths = []
+    for page_number in range(len(pdf_document)):
+        page = pdf_document[page_number]
+        image = page.get_pixmap()
+        image_path = os.path.join("pdf_images", f"page_{page_number}.png")
+        image.save(image_path)
+        image_paths.append(image_path)
 
-        # Get the selected page
-        page = doc[page_number]
-
-        # Render the page to an image
-        pixmap = page.get_pixmap()
-        
-        # Save the pixmap as a temporary PNG file
-        temp_image_path = f"temp_image.png"
-        pixmap.writePNG(temp_image_path)
-
-        # Display the image
-        st.image(Image.open(temp_image_path))
-
-    # Perform Arabic OCR on the selected page
-    image_path = f"page-{page_number}.png"
+    # Perform Arabic OCR on the images and extract text
+    extracted_text = []
     out_image = 'out.jpg'
-    results = arabicocr.arabic_ocr(image_path, out_image)
+    for image_path in image_paths:
+        results = arabicocr.arabic_ocr(image_path, out_image)
+        words = [result[1] for result in results]
+        text = " ".join(words)
+        extracted_text.append(text)
 
-    # Extract and display the text
-    words = [result[1] for result in results]
-    text = " ".join(words)
+    # Combine extracted text from all images
+    text = "\n".join(extracted_text)
 
-    # Display the text in the right column
-    with col2:
-        st.header("Extracted Text")
-        st.text(text)
-
-    # Save the extracted text as a text file
-    with open('file.txt', 'w', encoding='utf-8') as myfile:
-        myfile.write(text)
-
-    # Clean up the temporary PNG file
-    if os.path.exists(temp_image_path):
-        os.remove(temp_image_path)
-
-# Clean up the temporary PDF file after processing
-if os.path.exists("arabic_image.pdf"):
-    os.remove("arabic_image.pdf")
+    # Display the extracted text in a scrollable text area
+    st.header("Extracted Text")
+    st.text_area("Extracted Arabic Text", text, height=400)
